7efac0ed81fdc239bb76fb80eea8df41e8c856b7
JohanaC-137 <penalozajohana@gmail.com>
2021-03-10 02:44:10 -0300

    agregar

---
+++ imagen.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    
+    $("#btn1").click(function() {
+        $(this).fadeOut(3000);
+      });
+
+    $("#btn2").click(function() {
+        $(this).fadeOut(3000);
+    });
+    $("#btn3").click(function() {
+        $(this).fadeOut(3000);
+      });
+
+    $("#btn4").click(function() {
+        $(this).fadeOut(3000);
+    });
+    $("#btn5").click(function() {
+        $(this).fadeOut(3000);
+      });
+
+    $("#btn6").click(function() {
+        $(this).fadeOut(3000);
+    });
+    $("#btn7").click(function() {
+        $(this).fadeOut(3000);
+      });
+
+    $("#btn8").click(function() {
+        $(this).fadeOut(3000);
+    });
+    $("#btn9").click(function() {
+        $(this).fadeOut(3000);
+      });
+
+    $("#btn10").click(function() {
+        $(this).fadeOut(3000);
+    });
+});
+
+$("#mostrar").click(function() {
+  location.reload();
+});
